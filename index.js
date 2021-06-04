e09abdf4679d7cf10e9b7304039a2ef17fcef0e8
David Rafe <david.rafe@gmail.com>
2021-06-04 16:57:29 -0400

    commit tree e79a8fec4553fe35bc164ca04d4387d8cf332526

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 function superbowlWin(array) { 
-   let win = array.find(function(obj) {
+    let win = array.find(function(obj) { 
         if(obj.result === "W") {
             return obj.year 
         } else {
             return undefined 
         }
     })
-    return win 
+    return !!win ? win.year : undefined  
 }
 
+superbowlWin()
